b8ba669b5c7b1d6f7b2e253083c3503e9cdde860
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-02-13 18:50:03 +0600

    Data added

---
+++ src/components/data/services.jsx
@@ -0,0 +1,20 @@
+export const services = [
+  {
+    id: "baby",
+    name: "Baby Sitting Service",
+    price: 500,
+    description: "Professional babysitting service."
+  },
+  {
+    id: "elderly",
+    name: "Elderly Care Service",
+    price: 700,
+    description: "Care service for elderly people."
+  },
+  {
+    id: "sick",
+    name: "Sick People Care",
+    price: 800,
+    description: "Home care for sick patients."
+  }
+];
